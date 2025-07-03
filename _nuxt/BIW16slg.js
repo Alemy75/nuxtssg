cd7e9618fa2db1f57c3553fabe16cd0eaa88803d
Maxim Alekseev <max.alek@truelabel.io>
2025-07-03 17:31:03 +0300

    Updates

---
+++ _nuxt/BIW16slg.js
@@ -0,0 +1,1 @@
+import{_ as c,x as e,y as r}from"./CA4nIRYa.js";const t={};function o(n,s){return r(),e("div",null,"product")}const a=c(t,[["render",o]]);export{a as default};
---
+++ _nuxt/C3tAr-64.js
@@ -0,0 +1,1 @@
+import{_ as e,x as c,y as n}from"./CA4nIRYa.js";const r={};function s(t,o){return n(),c("h1",null,"This is SSG")}const a=e(r,[["render",s]]);export{a as default};
